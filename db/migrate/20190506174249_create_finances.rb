8298c0bf1d43a4565ddabf72443cc5779232288e
Wlademir Renan dos Santos <wlademir.renan@gmail.com>
2019-05-08 09:28:21 -0300

    ajust type of column

--- db/migrate/20190506174249_create_finances.rb
+++ db/migrate/20190506174249_create_finances.rb
@@ -2,7 +2,7 @@ class CreateFinances < ActiveRecord::Migration[5.0]
   def change
     create_table :finances do |t|
       t.integer :type, null: false
-      t.date :datetime, null: false
+      t.datetime :datetime, null: false
       t.float :value, null: false
       t.integer :cpf, null: false
       t.string :card, null: false
